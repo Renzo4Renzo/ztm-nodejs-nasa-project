7cd59715bffad22834779c1d47ad06d622396da4
Renzo Diaz <renzodiaz554@gmail.com>
2022-10-26 21:52:15 -0500

    Sorting GET launches and managing secrets with .env

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,19 +6,6 @@ const planets = require("./planets.schema");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: "Kepler Exploration X", //name
-  rocket: "Explorer IS1", //rocket.name
-  launchDate: new Date("December 27, 2030"), //date_local
-  target: "Kepler-442 b", // not applicable
-  customers: ["ZTM", "NASA"], //payload.customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = "https://api.spacexdata.com/v5/launches/query";
 
 async function populateLaunches() {
@@ -90,7 +77,7 @@ async function getLatestFlightNumber() {
 }
 
 async function getLaunches(skip, limit) {
-  return await launchesDatabase.find({}, { _id: 0, __v: 0 }).skip(skip).limit(limit);
+  return await launchesDatabase.find({}, { _id: 0, __v: 0 }).sort({ flightNumber: 1 }).skip(skip).limit(limit);
 }
 
 async function findLaunch(filter) {
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const http = require("http");
 const app = require("./app");
+
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 const { loadLaunchesData } = require("./models/launches.model");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.NASA_PORT || 8000;
 
 const server = http.createServer(app);
 
